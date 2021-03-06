f2beeeea6f540d0a65f698a1dd7285d4f1622dc6
Giovanni Medina <grablens_war@hotmail.com>
2021-03-06 12:19:03 -0600

    ya funca

--- app/src/main/java/com/giogen/footgame/MainActivity.java
+++ app/src/main/java/com/giogen/footgame/MainActivity.java
@@ -12,32 +12,40 @@ import android.util.Log;
 import android.view.Display;
 import android.widget.TextView;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor mSensor;
     int vx, vy, aceleracion= 10,px,py;
     float  inicialY, inicialX;
-    TextView lbBaloon;
+    TextView lbBaloon,lbAX,lbAX2,lbDX,lbDX2,lbGoles;
     Display display ;
-
+    int golArriba=0,golAbajo=0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         display = getWindowManager().getDefaultDisplay();
         sensorManager = (SensorManager) getSystemService(this.SENSOR_SERVICE);
         mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         lbBaloon = findViewById(R.id.lbBalon);
+        lbAX = findViewById(R.id.lbXA);
+        lbAX2 = findViewById(R.id.lbX2A);
+        lbDX = findViewById(R.id.lbDX);
+        lbDX2 = findViewById(R.id.lbDAX2);
+        lbGoles = findViewById(R.id.lbGoles);
+        lbGoles.setText("Goles:"+golArriba+"-"+golAbajo);
+
         Point size = new Point();
         display.getSize(size);
         px = size.x;
         py = size.y;
         inicialX = lbBaloon.getX();
         inicialY = lbBaloon.getY();
+        Log.d("CALIS","Y:"+py);
+        Log.d("CALIS","X:"+px);
 
 
 
@@ -48,28 +56,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
-        if (event.values[0]<0.1 & lbBaloon.getX()<px -lbBaloon.getWidth()){
-           vx+=5;
-            lbBaloon.setTranslationX(vx*aceleracion);
-        }else if(event.values[0]>0.1 & lbBaloon.getX()>0){
+        if (lbBaloon.getX()>lbAX.getX() & lbBaloon.getX()< lbAX2.getX() & lbBaloon.getY() >20 & lbBaloon.getY() <=27  ){
+            Log.d("CALIS","Gooool arribaaaaa");
+            vx=0;
+            vy=0;
+            lbBaloon.setY(inicialY);
+            lbBaloon.setX(inicialX);
+            golArriba++;
+            lbGoles.setText("Goles:"+golArriba+"-"+golAbajo);
 
-            vx-=5;
-            lbBaloon.setTranslationX(vx*aceleracion);
+        }else if (lbBaloon.getX()>lbDX.getX() & lbBaloon.getX()< lbDX2.getX() & lbBaloon.getY() >=py-400){
+            Log.d("CALIS","Gooool abajoooooooo");
+            vx=0;
+            vy=0;
+            lbBaloon.setY(inicialY);
+            lbBaloon.setX(inicialX);
+            golAbajo++;
+            lbGoles.setText("Goles:"+golArriba+"-"+golAbajo);
+
+        }else {
+            if (event.values[0] < 0.1 & lbBaloon.getX() < px - lbBaloon.getWidth()) {
+                vx += 5;
+                lbBaloon.setTranslationX(vx * aceleracion);
+            } else if (event.values[0] > 0.1 & lbBaloon.getX() > 0) {
+
+                vx -= 5;
+                lbBaloon.setTranslationX(vx * aceleracion);
+            }
+
+            if (event.values[1] > 0.1 & lbBaloon.getY() < py - 400) {
+                vy += 5;
+                lbBaloon.setTranslationY(vy * aceleracion);
+            } else if (event.values[1] < 0.1 & lbBaloon.getY() > 50) {
+
+                vy -= 5;
+                lbBaloon.setTranslationY(vy * aceleracion);
+            }
         }
 
-        if (event.values[1]>0.1 & lbBaloon.getY()<py-400){
-            vy+=5;
-            lbBaloon.setTranslationY(vy*aceleracion);
-        }else if(event.values[1]<0.1 & lbBaloon.getY()> 50){
-
-            vy-=5;
-            lbBaloon.setTranslationY(vy*aceleracion);
-        }
 
 
 
-        Log.d("CALIS","Y:"+lbBaloon.getY());
-        Log.d("CALIS","X:"+lbBaloon.getX());
 
 
     }
